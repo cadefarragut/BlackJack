1d942a78ae8c8a770a549ef63ee7f9811da36e83
cadefarragut <cade_farragut@yahoo.com>
2025-08-04 20:09:28 -0500

    Initial commit

---
+++ blackjack.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<string, int> getCards(){
+	map<string, int> deck;
+	deck["1"] = 4;
+	deck["2"] = 4;
+	deck["3"] = 4;
+	deck["4"] = 4;
+	deck["5"] = 4;
+	deck["6"] = 4;
+	deck["7"] = 4;
+	deck["8"] = 4;
+	deck["9"] = 4;
+	deck["10"] = 4;
+	deck["J"] = 4;
+	deck["Q"] = 4;
+	deck["K"] = 4;
+	deck["A"] = 4;
+
+
+	return deck;
+}
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+	return 0;
+}
