ee842b6f6906ed0ccd01f234c86cce17f1fea6ea
cadefarragut <cade_farragut@yahoo.com>
2025-08-04 21:29:31 -0500

    beggining

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,37 +1,80 @@
 #include <iostream>
 #include <map>
+#include <random>
+#include <string>
 using namespace std;
 
-map<string, int> getCards(){
-	map<string, int> deck;
-	deck["1"] = 4;
-	deck["2"] = 4;
-	deck["3"] = 4;
-	deck["4"] = 4;
-	deck["5"] = 4;
-	deck["6"] = 4;
-	deck["7"] = 4;
-	deck["8"] = 4;
-	deck["9"] = 4;
-	deck["10"] = 4;
-	deck["J"] = 4;
-	deck["Q"] = 4;
-	deck["K"] = 4;
-	deck["A"] = 4;
+map<string, int> deck = {
+	{"1", 4 },
+	{"2", 4 },
+	{"3", 4 },
+	{"4", 4 },
+	{"5", 4 },
+	{"6", 4 },
+	{"7", 4 },
+	{"8", 4 },
+	{"9", 4 },
+	{"10", 4 },
+	{"J", 4 },
+	{"Q", 4 },
+	{"K", 4 },
+	{"A", 4 }
+};
 
+bool isEmpty(){
+	for( const auto& [key, value] : deck) {
+		if(value != 0){
+	 	return false;
+		}
+	}
+	return true;
+		
+}
 
-	return deck;
+void dealCard(){
+
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> distr(1, 14);
+	if (isEmpty()){
+		cout << "Deck is Empty" << endl;
+		return ;
+	}
+
+	int card = distr(gen);
+	if( card < 11 ){
+		cout<< to_string(card);
+	}
+	else if (card == 11){
+		cout << "J";
+	}
+	else if (card == 12){
+		cout << "Q";
+	}
+	else if (card == 13){
+		cout << "K";
+	}
+	else{
+		cout << "A";
+	}
+	
+//	deck(to_string(card-1))--;
 }
 
 
-
-
-
-
-
-
-
 int main(){
+	
+	cout << "Welcome to BlackJack" <<endl;
+	cout << "Dealer: "; 
+	dealCard();
+	cout << endl;
+	cout << "You: "; 
+	dealCard(); 
+	cout<< " "; 
+	dealCard();
+	cout << endl;
+
+
 
 
 	return 0;
